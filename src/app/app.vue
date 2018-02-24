551da49aeae91099655df4ee0e3a61f757df5a5d
Rafal Galka <rafal.galka@modernweb.pl>
2018-02-24 09:57:17 +0100

    bootstrap-vue integration

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,3 +1,2 @@
 <script src="./app.js"></script>
 <template src="./app.html"></template>
-<style src="./app.scss" lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
 import App from './app/app';
 import router from './app/router/router';
+import './styles/main.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
