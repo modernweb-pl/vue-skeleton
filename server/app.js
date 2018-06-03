fe9f8e9451d38be19324d395825cfce07e42889c
Rafal Galka <rafal.galka@modernweb.pl>
2018-06-03 21:23:42 +0200

    [server] healthcheck

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const healthcheckRouter = require('./routes/healthcheck');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../dist/')));
 
+app.use('/healthcheck', healthcheckRouter);
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
