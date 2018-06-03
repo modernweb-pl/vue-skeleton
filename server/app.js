cda24e48a2f9d1d5f7333c2fa80428953369dc2c
Rafal Galka <rafal.galka@modernweb.pl>
2018-06-03 21:25:43 +0200

    [server] remove unused routes

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const healthcheckRouter = require('./routes/healthcheck');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const healthCheckRouter = require('./routes/healthcheck');
 
 const app = express();
 const isProduction = app.get('env') === 'production';
@@ -21,9 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../dist/')));
 
-app.use('/healthcheck', healthcheckRouter);
-app.use('/api', indexRouter);
-app.use('/api/users', usersRouter);
+app.use('/healthcheck', healthCheckRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
