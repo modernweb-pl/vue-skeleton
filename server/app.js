0f9c1a34e78cd4dd5254e9e853ad34abc6d726a9
Rafal Galka <rafal.galka@modernweb.pl>
2018-06-03 19:55:20 +0200

    [server] remove express views and serve `dist` folder instead

--- server/app.js
+++ server/app.js
@@ -14,33 +14,30 @@ if (isProduction) {
   app.set('x-powered-by', false);
 }
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger(isProduction ? 'tiny' : 'dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../dist/')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  let body = `<h1>${status} ${err.message}</h1>`;
+  if (!isProduction) {
+    body += `<pre>${err.stack}</pre>`;
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(status);
+  res.send(body);
 });
 
 module.exports = app;
