2033d3d2ae7ebe66559a8e150ffe4fbb4366e3db
Rafal Galka <rafal.galka@modernweb.pl>
2022-01-02 18:37:16 +0100

    feat(docker): run script
    + refactor to single cli script

--- .docker/push.js
+++
@@ -1,8 +0,0 @@
-const pkg = require('../package');
-const { spawn } = require('child_process');
-
-const docker = spawn('docker', ['push', pkg.docker.registry + '/' + pkg.name + ':' + pkg.version]);
-
-docker.stdout.on('data', (data) => console.log(data.toString()));
-docker.stderr.on('data', (data) => console.log(data.toString()));
-docker.on('exit', (code) => process.exit(code));
