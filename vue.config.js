dba1d97ce7032314059eeb399db8a66d4aa32863
Rafal Galka <rafal.galka@modernweb.pl>
2022-01-03 01:35:49 +0100

    feat(vue): setup transformAssetUrls for bootstrap-vue
    https://bootstrap-vue.org/docs/reference/images#vue-cli-3-support

--- vue.config.js
+++ vue.config.js
@@ -14,4 +14,27 @@ module.exports = {
       onlyProduction: true,
     },
   },
+  chainWebpack: (config) => {
+    /** @see https://bootstrap-vue.org/docs/reference/images#vue-cli-3-support */
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap((options) => {
+        options.transformAssetUrls = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-avatar': 'src',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src',
+        };
+
+        return options;
+      });
+  },
 };
